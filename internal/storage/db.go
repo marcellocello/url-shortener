a2ec905be02bae34a6e16bcac6785befb16d7279
Marcello Ryan <mrcello13@gmail.com>
2026-02-03 16:29:23 +0700

    add visit_count

--- internal/storage/db.go
+++ internal/storage/db.go
@@ -35,6 +35,7 @@ func InitDB() (*DB, error) {
     query := `CREATE TABLE IF NOT EXISTS urls (
         id SERIAL PRIMARY KEY,
         original_url TEXT NOT NULL,
+	visit_count INTEGER DEFAULT 0,
         short_code TEXT UNIQUE NOT NULL
     );`
 
